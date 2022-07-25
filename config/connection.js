12e96f553516570fcac3a6c6cc9f0d84015088d8
Edawgg <99540833+Edawgg@users.noreply.github.com>
2022-07-24 23:59:12 -0500

    config mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://mccannethan335:<Raptor123!>@cluster0.guscq.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            UseUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-
-module.exports = MongoClient.connection;
+module.exports = connectDB;
